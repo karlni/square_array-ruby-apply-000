3620195ca1e65386fe4a89ec5c334ff9df600e3b
agent.karl.zl.ni@gmail.com <agent.karl.zl.ni@gmail.com>
2018-10-16 18:02:51 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,8 @@
 def square_array(array)
-  # your code here
-  numbers = [1,2,3,4,5]
-  numbers.each {|x| print x*x}
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+my_arr = [1, 2]
+p square_array(my_arr) #=> [1, 4]
